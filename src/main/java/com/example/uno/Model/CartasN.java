e71d29c123c742eb3030015fccf10f96fa1c10de
Miguel Ángel Descance Peña <miguel.descance@correounivalle.com>
2025-05-16 11:31:14 -0500

    Esta es la versión final del juego, la cual presenta documentación completa

--- src/main/java/com/example/uno/Model/CartasN.java
+++ src/main/java/com/example/uno/Model/CartasN.java
@@ -1,6 +1,7 @@
 package com.example.uno.Model;
 
 /**
+ * Esta es una subclase de Cartas para las cartas numéricas (0–9).
  * @author Miguel Descance
  * @author Erick Obando
  * @version 1.0
@@ -15,6 +16,12 @@ public class CartasN extends Cartas{
         super(tipodecarta, color, ruta);
         this.numero = numero;
     }
+
+    /**
+     * Este método devuelve el valor numérico de la carta (0–9) para las cartas de tipo numérico.
+     * Se usa, por ejemplo, al comparar números iguales para ver si la carta puede jugarse sobre la pila .
+     * @return numero
+     */
 
     public int getNumero() {
         return numero;
--- src/main/java/com/example/uno/Model/Jugador.java
+++ src/main/java/com/example/uno/Model/Jugador.java
@@ -3,6 +3,10 @@ package com.example.uno.Model;
 import java.util.ArrayList;
 
 /**
+ * Esta clase es el modelo que representa a un jugador (humano o CPU).
+ * Mantiene una lista de cartas en el mazo.
+ * Inclute métodos para añadir y quitar cartas,
+ * consultar tamaño de la mano y obtener la lista.
  * @author Miguel Descance
  * @author Erick Obando
  * @version 1.0
@@ -12,19 +16,39 @@ public class Jugador {
 
     private  ArrayList<Cartas> mazo = new ArrayList();
 
-    //LAS SIGUIENTES FUNCIONES SE USAN EN LA REPARTICION DE CARTAS
+    /**
+     * Este método entrega la cantidad de cartas que tiene actualmente en mano el jugador,
+     * para detectar mazo vacío (victoria) o para iterar sobre sus cartas
+     * @return mazo.size
+     */
     public int mazoSize() {
         return mazo.size();
     }
 
+    /**
+     * Este método añade la carta indicada al final de la mano del jugador,
+     * por ejemplo al repartir o como penalización.
+     * @param carta
+     */
     public void addCarta(Cartas carta){
         mazo.add(carta);
     }
 
+    /**
+     * Este métooo devuelve la carta que está en la posición i de la mano,
+     * permitiendo inspeccionarla o jugarla.
+     * @param i
+     * @return mazo.get
+     */
     public Cartas getCarta(int i){
         return mazo.get(i);
     }
 
+    /**
+     * Este método elimina la primera aparición de esa carta de la mano del jugador,
+     * usado cuando el jugador o la CPU descarta o penaliza.
+     * @param carta
+     */
     public void removeCarta(Cartas carta){
         mazo.remove(carta);
     }
